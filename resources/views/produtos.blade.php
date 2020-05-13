e00988616e385e006f22710a59761d275a0d8e61
Dumon <dumon.dmd@gmail.com>
2020-05-13 17:18:39 -0300

    HTML - View Produto - Formulário para salvar Produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 <div class="card border">
 	<div class="card-body">
-		<h5 class="card-title">Cadastro de Produtos</h5>		
+		<h5 class="card-title">Cadastro de Produtos</h5>
 		<table class="table table-ordered table-hover">
 			<thead>
 				<tr>
@@ -16,14 +16,68 @@
 				</tr>
 			</thead>
 			<tbody>
-						
-			
+				
 			</tbody>
+			
 		</table>
 		
-	</div>	
-	<div class="card-footer">
-		<button class="btn btn-sm btn-primary" role="button">Novo produto</button>			
 	</div>
+	<div class="card-footer">
+		<button class="btn btn-sm btn-primary" role="button">Novo produto</button>
+	</div>
+	
 </div>
+
+<div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<form class="form-horizontal" id="formProduto">
+				<div class="modal-header">
+					<h5 class="modal-title">Novo produto</h5>					
+				</div>
+				<div class="modal-body">
+					<input type="hidden" id="id" class="form-control">
+					
+					<div class="form-group">
+						<label for="nomeProduto" class="control-label">Nome do produto</label>
+						<div class="input-group">
+							<input type="text" class="form-control" id="nomeProduto" placeholder="Nome do produto">
+						</div>						
+					</div>
+
+					<div class="form-group">
+						<label for="precoProduto" class="control-label">Preço</label>
+						<div class="input-group">
+							<input type="number" class="form-control" id="precoProduto" placeholder="Preço do produto">
+						</div>						
+					</div>
+
+					<div class="form-group">
+						<label for="quantidadeProduto" class="control-label">Quantidade</label>
+						<div class="input-group">
+							<input type="number" class="form-control" id="quantidadeProduto" placeholder="Quantidade do produto">
+						</div>						
+					</div>
+
+					<div class="form-group">
+						<label for="departamentoProduto" class="control-label">Departamento</label>
+						<div class="input-group">
+							<input type="text" class="form-control" id="departamentoProduto" placeholder="Departamento do produto">
+						</div>						
+					</div>
+					
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary">Salvar</button>
+					<button type="cancel" class="btn btn-danger" data-dissmiss="modal">Cancelar</button>
+				</div>
+			</form>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
+
 @endsection	
